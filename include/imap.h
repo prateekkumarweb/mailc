d376e8c6261dcb388f40c718d1b81c500a69d293
Prateek Kumar <prateek@prateekkumar.in>
2018-04-21 10:55:44 +0530

    Basic functions to be implemented listed in imap.h
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- include/imap.h
+++ include/imap.h
@@ -1,6 +1,10 @@
 #ifndef IMAP_H
 #define IMAP_H
 
+class Mail {
+	// ?
+};
+
 class IMAPConnection {
 public:
 	IMAPConnection(std::string &hostname, int port);
@@ -9,7 +13,29 @@ public:
 	bool noop(); // OK, BAD
 	bool logout();
 
-	
+	std::vector<Mail> getUnseenMails(const std::string &mailbox);
+
+	std::vector<Mail> getTopMails(const std::string &mailbox, int k);
+
+	bool deleteMail(Mail mail);
+
+	int getUnseenCount(const std::string &mailbox);
+
+	int getTotalCount(const std::string &mailbox);
+
+	int getRecentCount(const std::string &mailbox);
+
+	bool createMailbox(std::string &mailbox);
+
+	bool renameMailbox(std::string &mailbox);
+
+	bool deleteMailbox(std::string &mailbox);
+
+	Mail getMail(... uid);
+
+	std::vector<Mail> search(???);
+
+	std::vector<??> listFolders(const std::string &mailbox, ?? (Wildcard));
 
 	~IMAPConnection();
 private:
